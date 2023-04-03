e33f3059fac1f30cfcedb201e7ece3b03e5bd31b
Kameron Watts <kam.watts@gmail.com>
2023-04-03 15:43:39 -0700

    fixed package test

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const app = require('./server');
 
-const PORT = process.env.PORT || 3002
-
 app.listen(3002, () => {
   console.log('App is listening to you... And watching...On PORT: 3002')
 });
